822cc453ce1377e0cd91c07e157ee1c8a6e58012
LianJun <1586008023@qq.com>
2025-01-19 01:19:57 +0800

    Test

--- README.md
+++
@@ -1,2 +0,0 @@
-# LianJun 的个人主页仓库
-前往 [LianJun 主页](https://lianjun.me "LianJun 的个人主页")
--- js/backButton.js
+++
@@ -1,6 +0,0 @@
-const backButtonLianJun = document.querySelectorAll(".backButton-LianJun")
-backButtonLianJun.forEach(element => {
-    element.addEventListener("click", function () {
-        window.location.assign("../index.html")
-    })
-});
